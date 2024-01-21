2c0d4601cb8b9f5d1a1680e126f404c9ee8ff5e5
Isaac Turner <spacey_sooty@outlook.com>
2024-01-21 20:39:15 +0800

    Initial Commit

---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,32 @@
+#include "Robot.h"
+
+void Robot::RobotInit() {
+  m_motor = new ctre::phoenix6::hardware::TalonFX(99, "Drivebase");
+  m_encoder = new ctre::phoenix6::hardware::CANcoder(99, "Drivebase");
+  m_pid = new frc::PIDController(0.022, 0, 0);
+}
+void Robot::RobotPeriodic() {}
+
+void Robot::AutonomousInit() {}
+void Robot::AutonomousPeriodic() {}
+
+void Robot::TeleopInit() {}
+void Robot::TeleopPeriodic() {
+  m_pid->SetSetpoint(60);
+  m_motor->SetVoltage(units::volt_t{m_pid->Calculate(m_encoder->GetPosition().GetValue().value())});
+}
+
+void Robot::DisabledInit() {}
+void Robot::DisabledPeriodic() {}
+
+void Robot::TestInit() {}
+void Robot::TestPeriodic() {}
+
+void Robot::SimulationInit() {}
+void Robot::SimulationPeriodic() {}
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
---
+++ src/main/include/Robot.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <frc/TimedRobot.h>
+#include <frc/controller/PIDController.h>
+#include <ctre/phoenix6/TalonFX.hpp>
+#include <ctre/phoenix6/CANcoder.hpp>
+
+class Robot : public frc::TimedRobot {
+ public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+  void TestInit() override;
+  void TestPeriodic() override;
+  void SimulationInit() override;
+  void SimulationPeriodic() override;
+
+ private:
+  ctre::phoenix6::hardware::TalonFX* m_motor;
+  ctre::phoenix6::hardware::CANcoder* m_encoder;
+  frc::PIDController* m_pid;
+};
