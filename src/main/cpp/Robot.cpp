44d93a80f68fe53b779a963b034bd1d64933fd78
Isaac Turner <spacey_sooty@outlook.com>
2024-01-22 11:48:14 +0800

    from scratch drive

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -5,8 +5,6 @@ static double preverror = 0;
 
 void Robot::RobotInit() {
   m_motor = new ctre::phoenix6::hardware::TalonFX(7, "Drivebase");
-  m_encoder = new ctre::phoenix6::hardware::CANcoder(16, "Drivebase");
-  //m_pid = new frc::PIDController(0.022, 0, 0);
 }
 void Robot::RobotPeriodic() {}
 
@@ -18,15 +16,13 @@ void Robot::TeleopPeriodic() {
   double currenttimestamp = frc::Timer::GetFPGATimestamp().value();
   double dt = currenttimestamp - lasttime;
   double setpoint = 60;
-  double input = m_encoder->GetPosition().GetValue().value();
+  double input = m_motor->GetPosition().GetValue().value();
   double error = setpoint - input;
   double deriv = error - preverror / dt;
   double sum = error * dt;
   double output = 0.001 * error + 0 * sum + 0 * deriv + 0.1 * input;
   m_motor->SetVoltage(units::volt_t{output});
 
-  //m_pid->SetSetpoint(60);
-  //m_motor->SetVoltage(units::volt_t{m_pid->Calculate(m_encoder->GetPosition().GetValue().value())});
   lasttime = currenttimestamp;
   preverror = error;
 }
